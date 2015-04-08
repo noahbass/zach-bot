5a7e9e1d5939da02c5f56eac13e831d126cff056
Noah Bass <bassjnoah@gmail.com>
2015-04-08 12:13:29 -0400

    added what are we doing as trigger

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/what$/;
+      //botRegex = /^\/what$/;
+      botRegex = /what are we doing/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);

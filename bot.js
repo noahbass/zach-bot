7fba213d49e9c7c46a7aee3cb0cff6ce13377ca5
Noah Bass <bassjnoah@gmail.com>
2015-04-08 12:59:22 -0400

    cleaned up zach-bot

--- bot.js
+++ bot.js
@@ -1,59 +1,59 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      //botRegex = /^\/what$/;
-      botRegex = /what are we doing/,
-      botRegex2 = /What are we doing/;
+    var request = JSON.parse(this.req.chunks[0]),
+        //botRegex = /^\/what$/;
+        botRegex = /what are we doing/,
+        botRegex2 = /What are we doing/;
 
-  if(request.text && (botRegex.test(request.text) || botRegex2.test(request.text))) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
+    if(request.text && (botRegex.test(request.text) || botRegex2.test(request.text))) {
+        this.res.writeHead(200);
+        postMessage();
+        this.res.end();
+    } else {
+        console.log("don't care");
+        this.res.writeHead(200);
+        this.res.end();
+    }
 }
 
 function postMessage() {
-  var botResponse, options, body, botReq;
+    var botResponse, options, body, botReq;
 
-  botResponse = 'Your mom';
+    botResponse = 'Your mom';
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
+    options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+    };
 
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
+    body = {
+        "bot_id": botID,
+        "text": botResponse
+    };
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+    console.log('sending ' + botResponse + ' to ' + botID);
 
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
+    botReq = HTTPS.request(options, function(res) {
+        if(res.statusCode == 202) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+    botReq.on('error', function(err) {
+        console.log('error posting message '  + JSON.stringify(err));
+    });
+
+    botReq.on('timeout', function(err) {
+        console.log('timeout posting message '  + JSON.stringify(err));
+    });
+
+    botReq.end(JSON.stringify(body));
 }
-
 
 exports.respond = respond;
--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-var http, director, cool, bot, router, server, port;
+var http, director, bot, router, server, port;
 
 http        = require('http');
 director    = require('director');
-cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
 router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
-  }
+    '/' : {
+        post: bot.respond,
+        get: ping
+    }
 });
 
 server = http.createServer(function (req, res) {
-  req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
-  });
+    req.chunks = [];
+    req.on('data', function (chunk) {
+        req.chunks.push(chunk.toString());
+    });
 
-  router.dispatch(req, res, function(err) {
-    res.writeHead(err.status, {"Content-Type": "text/plain"});
-    res.end(err.message);
-  });
+    router.dispatch(req, res, function(err) {
+        res.writeHead(err.status, {"Content-Type": "text/plain"});
+        res.end(err.message);
+    });
 });
 
 port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
-  this.res.writeHead(200);
-  this.res.end("Hey, I'm Cool Guy.");
+    this.res.writeHead(200);
+    this.res.end('Sup! This is a groupme bot!');
 }

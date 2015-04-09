8c7188de9ae997502d32c1f1abe772284f51ef4e
Noah Bass <bassjnoah@gmail.com>
2015-04-08 22:11:15 -0400

    updated bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 function respond() {
     var request = JSON.parse(this.req.chunks[0]),
         //botRegex = /^\/what$/;
-        botRegex = /What are we doing/i;
+        botRegex = /What are (we|you) doing/i;
 
     if(request.text && botRegex.test(request.text)) {
         this.res.writeHead(200);
